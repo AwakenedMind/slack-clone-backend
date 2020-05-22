835b5d339396358704ae22f824d826728541f254
Robert Arteaga <robertarteaga@Roberts-iMac.local>
2020-05-22 02:21:43 -0700

    added merging

--- index.js
+++ index.js
@@ -1,33 +1,42 @@
-const {
-	ApolloServer,
-	gql,
-	makeExecutableSchema,
-} = require('apollo-server-express');
-const { resolvers } = require('./resolvers');
-const { typeDefs } = require('./schema');
+import { ApolloServer, makeExecutableSchema } from 'apollo-server-express';
 import models from './models/index';
+import express from 'express';
+import path from 'path';
+import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
 
-const express = require('express');
-const app = express();
+// Merge type defs and resolvers
+const typeDefs = mergeTypes(fileLoader(path.join(__dirname, './schema')));
+const resolvers = mergeResolvers(
+	fileLoader(path.join(__dirname, './resolvers'))
+);
 
-// combine resolvers and type definitions into a schema
+// Combine resolvers and type definitions into a schema
 export const schema = makeExecutableSchema({
 	typeDefs,
 	resolvers,
 });
 
 // Create the server
+const app = express();
 const server = new ApolloServer({
 	typeDefs,
 	resolvers,
 	playground: true,
+	context: {
+		models,
+		user: {
+			id: 1,
+		},
+	},
 });
 
+// Apply express middleware
 server.applyMiddleware({ app });
 
+// Sync local postgresql db & start app in localhost:8080
 models.sequelize
-	.sync({ force: true })
-	.then((x) =>
+	.sync()
+	.then(() =>
 		app.listen({ port: 8080 }, () =>
 			console.log(
 				`🚀 Server ready at http://localhost:8080${server.graphqlPath}`
