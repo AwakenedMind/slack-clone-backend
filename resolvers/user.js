5053b3748791d081a09d546b05719a00abded3b3
Robert Arteaga <robertarteaga@Roberts-iMac.local>
2020-05-31 01:55:03 -0700

    added register mutation + hashing logic

--- resolvers/user.js
+++ resolvers/user.js
@@ -1,4 +1,13 @@
-import models from '../models';
+import bcrypt from 'bcrypt';
+import _ from 'lodash';
+
+const formatErrors = (e, models) => {
+	console.log(models.sequelize);
+	if (e instanceof models.Sequelize.ValidationError) {
+		return e.errors.map((x) => _.pick(x, ['path', 'message']));
+	}
+	return [{ path: 'name', message: 'something went wrong' }];
+};
 
 export default {
 	Query: {
@@ -7,8 +16,39 @@ export default {
 		allUsers: (parent, args, { models }) => models.User.findAll(),
 	},
 	Mutation: {
-		createUser: (parent, args, context) => {
-			return models.User.create(args);
+		register: async (parent, { password, ...otherArgs }, { models }) => {
+			try {
+				if (password.length > 32 || password.length < 5) {
+					console.log(otherArgs);
+					let passwordError = [
+						{
+							path: 'password',
+							message: 'The password needs to be between 5 and 32 characters',
+						},
+					];
+					return {
+						ok: false,
+						// errors: formatErrors([...passwordError, ...otherArgs], models)
+						errors: passwordError,
+					};
+				}
+
+				const hashedPassword = await bcrypt.hash(password, 12);
+				const user = await models.User.create({
+					...otherArgs,
+					password: hashedPassword,
+				});
+
+				return {
+					ok: true,
+					user,
+				};
+			} catch (error) {
+				return {
+					ok: false,
+					errors: formatErrors(error, models),
+				};
+			}
 		},
 	},
 };
