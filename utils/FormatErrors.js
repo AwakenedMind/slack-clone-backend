9f7fd5ae98ac98f49f12eb52378662fbe147f0ac
Robert Arteaga <robertarteaga@Roberts-iMac.local>
2020-06-03 00:37:41 -0700

    format error

--- utils/FormatErrors.js
+++ utils/FormatErrors.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 
 export const formatErrors = (e, models) => {
-	if (e instanceof models.Sequelize.ValidationError) {
+	if (e.errors.length > 0) {
 		return e.errors.map((x) => _.pick(x, ['path', 'message']));
 	}
 	return [{ path: 'name', message: 'something went wrong' }];
